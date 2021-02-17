f65483dd727d7da25b9ec7a33e7d55b69d0e264e
Michael Evangelos Diamantis <michaeldiamantis17@outlook.com>
2021-02-17 23:58:23 +0200

    First task complete

--- exer3_pg13.cpp
+++ exer3_pg13.cpp
@@ -2,6 +2,20 @@
 
 void SumNumbersFrom(int start, int end)
 {
+	if (start >= end)
+	{
+		std::cout << "End number is less than Start." << std::endl;
+		return;
+	}
+
+	int sum = 0, val = start;
+	while (val <= end)
+	{
+		sum += val;
+		++val;
+	}
+
+	std::cout << "Sum of " << start << " to " << end << " is " << sum << std::endl;
 
 }
 
@@ -17,13 +31,13 @@ void PrintNumbersBetween(int start, int end)
 
 int main()
 {
-	SumNumbersFrom(50,100);
+	SumNumbersFrom(50,100); //Task 1.9
 	system("pause");
 
-	DecrementNumbers();
+	DecrementNumbers(); //Task 1.10
 	system("pause");
 
-	PrintNumbersBetween(1,30);
+	PrintNumbersBetween(1,30); //Task 1.11
 
 	return 0;
 }
