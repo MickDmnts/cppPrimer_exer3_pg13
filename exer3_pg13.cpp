2e15611745273e78d2e163759b088e6fa68f3636
Michael Evangelos Diamantis <michaeldiamantis17@outlook.com>
2021-02-17 23:48:01 +0200

    Add project files.

---
+++ exer3_pg13.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+void SumNumbersFrom(int start, int end)
+{
+
+}
+
+void DecrementNumbers()
+{
+
+}
+
+void PrintNumbersBetween(int start, int end)
+{
+
+}
+
+int main()
+{
+	SumNumbersFrom(50,100);
+	system("pause");
+
+	DecrementNumbers();
+	system("pause");
+
+	PrintNumbersBetween(1,30);
+
+	return 0;
+}
